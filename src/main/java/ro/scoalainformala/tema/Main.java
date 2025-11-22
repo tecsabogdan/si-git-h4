924aa0a201a082b80363dd33af05ea9231050e9c
TECSA\tecsa <tecsa.bogdan23@gmail.com>
2025-11-22 19:33:04 +0200

    test commit.

--- src/main/java/ro/scoalainformala/tema/Main.java
+++ src/main/java/ro/scoalainformala/tema/Main.java
@@ -4,9 +4,10 @@ package ro.scoalainformala.tema;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
         System.out.println("This is a new line.");
         System.out.println("Another line here.");
+        System.out.println("Yet another line.");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
