563f04791e466d845777ca9df53fd727c763cfa7
TECSA\tecsa <tecsa.bogdan23@gmail.com>
2025-11-22 19:07:04 +0200

    first commit

---
+++ src/main/java/ro/scoalainformala/tema/Main.java
@@ -0,0 +1,14 @@
+package ro.scoalainformala.tema;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.printf("Hello and welcome!");
+        System.out.println("This is a new line.");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
